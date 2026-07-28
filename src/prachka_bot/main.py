4abc2a0804076334241e17fe1b335705ee113d98
zaranka <zaranka-14@mail.ru>
2026-07-28 20:14:15 +0200

    fixing old deploy issues...

--- src/prachka_bot/main.py
+++ src/prachka_bot/main.py
@@ -1,15 +1,7 @@
-
-
 import sys
 import os
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-while current_dir != os.path.dirname(current_dir):
-    if os.path.exists(os.path.join(current_dir, "pyproject.toml")):
-        if current_dir not in sys.path:
-            sys.path.insert(0, current_dir)
-        break
-    current_dir = os.path.dirname(current_dir)
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
 import asyncio
 import logging
